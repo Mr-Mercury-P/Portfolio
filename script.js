4801bdb14c6fcd6257d3e1bf295c7e76a0b5d4d6
Puli Prabhas Reddy <141412556+Mr-Mercury-P@users.noreply.github.com>
2024-07-05 15:31:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("contactForm").addEventListener("submit", function(event) {
         event.preventDefault();
 
-        var name = document.getElementById("nameInput").value;
-        var email = document.getElementById("emailInput").value;
-        var message = document.getElementById("messageInput").value;
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
 
         alert("Form submitted successfully!\nName: " + name + "\nEmail: " + email + "\nMessage: " + message);
     });
